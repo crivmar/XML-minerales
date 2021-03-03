378442d6ede7a4a18f53a150b52c910f77de364b
crivmar <carlosrivero1988@gmail.com>
2021-03-03 12:46:02 +0100

    Añadido en funciones el inicio del  segundo ejercicio, en programa creado menu que no funciona bien porque está mal programado

--- funciones.py
+++ funciones.py
@@ -5,10 +5,12 @@ def leer_documento(d):
         l=etree.parse(d)
     return l
 
-def listar_informacion (l):
+def listar_informacion(l):
     n=l.xpath("//name/text()")
     u=l.xpath("//image/text()")
     for i in range (len(n)):
         n[i]=n[i]+","+" "+"puedes ver su imagen en: " +u[i]
     return n
 
+# def contar_informacion(l):
+#     n=l.xpath("//information/group/text()")
--- programa.py
+++ programa.py
@@ -2,8 +2,26 @@ from funciones import *
 
 arbol=leer_documento("/home/crivero/Descargas/Git/XML-minerales/Minerals.xml")
 
-## Ejercicio 1 ##
+menu='''
+1. Nombre e imagenes de los minerales.
+2. Nº de minerales por grupo.
+3. Clasificación Strunz.
+4. Categoria mineral.
+5. Elementos químicos.
+6. Salir'''
 
-for i in listar_informacion(arbol):
-    print(i)
-    print(" ")
+opcion=int(input("Introduce un número de 1 al 5, 6 para salir: "))
+
+while opcion!=:
+
+    ## Ejercicio 1 ##
+    if opcion=="1":
+        for i in listar_informacion(arbol):
+            print(i)
+            print(" ")
+    ## Ejercicio 2 ##
+
+    print(menu)
+    print(opcion)
+
+
