187ab10a00d50b0e648222ada427572aeb33b89c
crivmar <carlosrivero1988@gmail.com>
2021-03-04 09:28:28 +0100

    Añadida función menú

--- funciones.py
+++ funciones.py
@@ -1,5 +1,21 @@
 from lxml import etree
 
+def menu():
+    print('''
+    1. Nombre e imagenes de los minerales.
+    2. Nº de minerales por grupo.
+    3. Clasificación Strunz.
+    4. Categoria mineral.
+    5. Elementos químicos.
+    6. Salir
+    ''')
+    opcion=int(input("Elige una opción: "))
+    print("-"*40)
+    while opcion<1 or opcion>6:
+        print("Error")
+        opcion=int(input("Elige una opción: "))
+    return opcion
+
 def leer_documento(d):
     with open(d) as f:
         l=etree.parse(d)
--- programa.py
+++ programa.py
@@ -2,26 +2,16 @@ from funciones import *
 
 arbol=leer_documento("/home/crivero/Descargas/Git/XML-minerales/Minerals.xml")
 
-menu='''
-1. Nombre e imagenes de los minerales.
-2. Nº de minerales por grupo.
-3. Clasificación Strunz.
-4. Categoria mineral.
-5. Elementos químicos.
-6. Salir'''
-
-opcion=int(input("Introduce un número de 1 al 5, 6 para salir: "))
-
-while opcion!=:
-
+m=0
+while m!=6:
     ## Ejercicio 1 ##
-    if opcion=="1":
+    if m==1:
         for i in listar_informacion(arbol):
             print(i)
             print(" ")
     ## Ejercicio 2 ##
 
-    print(menu)
-    print(opcion)
+   
+print("Fin del programa.")
 
 
