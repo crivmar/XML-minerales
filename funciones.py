b159b33a797d52a2913a2ee3fbc8186d46c2302a
crivmar <carlosrivero1988@gmail.com>
2021-03-04 10:58:51 +0100

    Añadido ejercicio 2 en programa y función contar_información

--- funciones.py
+++ funciones.py
@@ -28,5 +28,58 @@ def listar_informacion(l):
         n[i]=n[i]+","+" "+"puedes ver su imagen en: " +u[i]
     return n
 
-# def contar_informacion(l):
-#     n=l.xpath("//information/group/text()")
+def contar_informacion(l):
+    dic1={}
+    O=0
+    Ar=0
+    Sulf=0
+    Sil=0
+    Pho=0
+    Car=0
+    Sulfa=0
+    Hal=0
+    Bor=0
+    Ot=0
+    Ox=0
+    Nae=0
+    n=l.xpath("//information/group/text()")
+    for i in n:
+        if i=='Organic Minerals':
+            O+=1
+            dic1["Minerales Orgánicos"]=O
+        elif i=='Arsenates':
+            Ar+=1
+            dic1["Arseniato"]=Ar
+        elif i=='Sulfides':
+            Sulf+=1
+            dic1["Sulfuros"]=Sulf
+        elif i=='Silicates':
+            Sil+=1
+            dic1["Silicatos"]=Sil
+        elif i=='Phosphates':
+            Pho+=1
+            dic1["Fosfatos"]=Pho
+        elif i=='Carbonates':
+            Car+=1
+            dic1["Carbonatos"]=Car
+        elif i=='Sulfates':
+            Sulfa+=1
+            dic1["Sulfatos"]=Sulfa
+        elif i=='Halides':
+            Hal+=1
+            dic1["Haluros"]=Hal
+        elif i=='Borates':
+            Bor+=1
+            dic1["Boratos"]=Bor
+        elif i=='Other':
+            Ot+=1
+            dic1["Otros"]=Ot
+        elif i=='Oxides':
+            Ox+=1
+            dic1["Óxidos"]=Ox
+        elif i=='Native Elements':
+            Nae+=1
+            dic1["Elementos Nativos"]=Nae
+                    
+    return dic1
+    
--- programa.py
+++ programa.py
@@ -4,14 +4,18 @@ arbol=leer_documento("/home/crivero/Descargas/Git/XML-minerales/Minerals.xml")
 
 m=0
 while m!=6:
+    m=menu()
     ## Ejercicio 1 ##
     if m==1:
         for i in listar_informacion(arbol):
             print(i)
             print(" ")
     ## Ejercicio 2 ##
+    if m==2:
+        for i,j in contar_informacion(arbol).items():
+            print(i,":",j)
 
-   
+
 print("Fin del programa.")
 
 
